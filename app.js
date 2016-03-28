43bae32464bdd344a03d1c719c326dc578242339
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2016-03-28 11:35:58 -0700

    Work work

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
+// https://api.github.com/users/RyanCavanaugh
+function go(username) {
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", draw);
+    oReq.open("GET", "https://api.github.com/users/" + username);
+    oReq.send();
+    function draw() {
+        var data = JSON.parse(this.responseText);
+        var output = new Image();
+        var logo = new Image();
+        var url = window.location.href;
+        var src = data.avatar_url;
+        var logoSrc = 'logo.png';
+        output.crossOrigin = "Anonymous";
+        output.onload = function () {
+            var size = 400;
+            var cv = document.getElementById("cv");
+            var ctx = cv.getContext('2d');
+            ctx.drawImage(output, 0, 0, size, size);
+            ctx.fillStyle = ctx.strokeStyle = 'rgb(0, 116, 193)';
+            ctx.lineWidth = size * 24 / 400;
+            ctx.strokeRect(0, 0, size, size);
+            var logoSize = 100;
+            var logoBottomMargin = 2;
+            var logoRightMargin = 2;
+            var arcSize = logoSize * 1.05;
+            ctx.arc(size, size, arcSize, 0, Math.PI * 2, false);
+            ctx.clip();
+            ctx.fillRect(size - arcSize, size - arcSize, arcSize, arcSize);
+            ctx.drawImage(logo, size - logoSize - logoRightMargin, size - logoSize - logoBottomMargin, logoSize, logoSize);
+        };
+        logo.onload = function () {
+            output.src = src;
+        };
+        logo.src = logoSrc;
+    }
+}
 document.addEventListener("DOMContentLoaded", function () {
-    var output = new Image();
-    var logo = new Image();
-    // let src = "http://avatars2.githubusercontent.com/u/7121557"; // insert image url here
-    var src = 'https://avatars0.githubusercontent.com/u/6685088?v=3&s=460';
-    var logoSrc = 'logo.png';
-    output.crossOrigin = "Anonymous";
-    output.onload = function () {
-        var size = 400;
-        var cv = document.getElementById("cv");
-        var ctx = cv.getContext('2d');
-        ctx.drawImage(output, 0, 0, size, size);
-        ctx.fillStyle = ctx.strokeStyle = 'rgb(29, 39, 64)';
-        ctx.lineWidth = size * 32 / 400;
-        ctx.strokeRect(0, 0, size, size);
-        var logoSize = 100;
-        var logoBottomMargin = 8;
-        var arcSize = logoSize * 1.6;
-        ctx.arc(size, size, arcSize, 0, Math.PI * 2, false);
-        ctx.clip();
-        ctx.fillRect(size - arcSize, size - arcSize, arcSize, arcSize);
-        ctx.drawImage(logo, size - logoSize, size - logoSize - logoBottomMargin, logoSize, logoSize);
-    };
-    logo.onload = function () {
-        output.src = src;
-    };
-    logo.src = logoSrc;
+    document.getElementById("username").addEventListener("keydown", function (e) {
+        if (!e) {
+            e = window.event;
+        }
+        if (e.keyCode == 13) {
+            submit();
+            e.preventDefault();
+        }
+    }, false);
+    document.getElementById('submit').onclick = submit;
+    function submit() {
+        go(document.getElementById('username').value);
+    }
 });
--- app.ts
+++ app.ts
@@ -1,37 +1,65 @@
+// https://api.github.com/users/RyanCavanaugh
+
+function go(username: string) {
+	var oReq = new XMLHttpRequest();
+	oReq.addEventListener("load", draw);
+	oReq.open("GET", "https://api.github.com/users/" + username);
+	oReq.send();
+
+	function draw() {
+		const data = JSON.parse(this.responseText);
+		let output = new Image();
+		let logo = new Image();
+		let url = window.location.href;
+		let src = data.avatar_url;
+		let logoSrc = 'logo.png';
+
+		output.crossOrigin = "Anonymous";
+
+		output.onload = () => {
+			let size = 400;
+
+			let cv = <HTMLCanvasElement>document.getElementById("cv");
+			let ctx = <CanvasRenderingContext2D>cv.getContext('2d');
+
+			ctx.drawImage(output, 0, 0, size, size);
+
+			ctx.fillStyle = ctx.strokeStyle = 'rgb(0, 116, 193)';
+			ctx.lineWidth = size * 24/400;
+			ctx.strokeRect(0, 0, size, size);
+
+			let logoSize = 100;
+			let logoBottomMargin = 2;
+			let logoRightMargin = 2;
+			let arcSize = logoSize * 1.05;
+			ctx.arc(size, size, arcSize, 0, Math.PI*2, false);
+			ctx.clip();
+			ctx.fillRect(size - arcSize, size - arcSize, arcSize, arcSize);
+			ctx.drawImage(logo, size - logoSize - logoRightMargin, size - logoSize - logoBottomMargin, logoSize, logoSize);
+		};
+
+		logo.onload = () => {
+			output.src = src;
+		};
+		logo.src = logoSrc;
+	}
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-	let output = new Image();
-	let logo = new Image();
-	// let src = "http://avatars2.githubusercontent.com/u/7121557"; // insert image url here
-	let src = 'https://avatars0.githubusercontent.com/u/6685088?v=3&s=460';
-	let logoSrc = 'logo.png';
+	document.getElementById("username").addEventListener("keydown", function(e) {
+    	if (!e) { e = window.event as KeyboardEvent; }
 
-	output.crossOrigin = "Anonymous";
+	    if (e.keyCode == 13) {
+	    	submit();
+	    	e.preventDefault();
+		}
+	}, false);
 
-	output.onload = () => {
-		let size = 400;
+	document.getElementById('submit').onclick = submit;
 
-		let cv = <HTMLCanvasElement>document.getElementById("cv");
-		let ctx = <CanvasRenderingContext2D>cv.getContext('2d');
-
-		ctx.drawImage(output, 0, 0, size, size);
-
-		ctx.fillStyle = ctx.strokeStyle = 'rgb(29, 39, 64)';
-		ctx.lineWidth = size * 32/400;
-		ctx.strokeRect(0, 0, size, size);
-
-		let logoSize = 100;
-		let logoBottomMargin = 8;
-		let arcSize = logoSize * 1.6;
-		ctx.arc(size, size, arcSize, 0, Math.PI*2, false);
-		ctx.clip();
-		ctx.fillRect(size - arcSize, size - arcSize, arcSize, arcSize);
-		ctx.drawImage(logo, size - logoSize, size - logoSize - logoBottomMargin, logoSize, logoSize);
-	};
-
-	logo.onload = () => {
-		output.src = src;
-	};
-	logo.src = logoSrc;
+	function submit() {
+		go((document.getElementById('username') as HTMLInputElement).value);		
+	}
 });
 
 
